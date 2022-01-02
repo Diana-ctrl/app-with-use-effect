d6cb1cb5c64628cb78384908dd4a365067b47c73
Diana <diana.lyah.96@mail.ru>
2022-01-02 13:19:47 +0300

    adding user search by login

--- src/Github.js
+++ src/Github.js
@@ -5,6 +5,8 @@ import axios from 'axios';
 export const Github = () => {
   const [selectidUser, setSelectedUser] = useState(null);
   const [users, setUsers] = useState([]);
+  const [tempSearch, setTempSearch] = useState('');
+
   useEffect(() => { 
      if(selectidUser) {
         document.title = selectidUser.login;
@@ -15,7 +17,7 @@ export const Github = () => {
     axios.get('https://api.github.com/search/users?q=it-kamasutra')
     .then(res=> {
       console.log(res)
-      setUser(res.data.items)
+      setUsers(res.data.items)
     })
 
   }, [])
@@ -25,8 +27,15 @@ export const Github = () => {
   return (
     <div className={s.container}>
       <div>
-        <input placeholder='Search' />
-        <button>find</button>
+        <input placeholder='Search' value={tempSearch} onChange={(e) => {setTempSearch(e.currentTarget.value)}}/>
+        <button onClick={() => {
+           axios.get(`https://api.github.com/search/users?${tempSearch}`)
+           .then(res=> {
+             console.log(res)
+             setUsers(res.data.items)
+           })
+
+        }}>find</button>
       </div>
 
       <ul>
@@ -41,6 +50,7 @@ export const Github = () => {
       <div>
         <h2>User name</h2>
         <div>Ditails</div>
+        <div>{tempSearch}</div>
       </div>
 
     </div>
