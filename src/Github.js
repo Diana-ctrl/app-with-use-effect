ddca0892cf862d8a7ece0107cbe2fc9f8fe43197
Diana <diana.lyah.96@mail.ru>
2022-01-02 13:04:56 +0300

    add users whis useEffect

--- src/Github.js
+++ src/Github.js
@@ -1,13 +1,26 @@
 import s from './github.module.css';
 import { useState, useEffect } from 'react';
+import axios from 'axios';
 
 export const Github = () => {
   const [selectidUser, setSelectedUser] = useState(null);
+  const [users, setUsers] = useState([]);
   useEffect(() => { 
      if(selectidUser) {
-        document.title = selectidUser;
+        document.title = selectidUser.login;
       }
   }, [selectidUser])
+
+  useEffect(() => {
+    axios.get('https://api.github.com/search/users?q=it-kamasutra')
+    .then(res=> {
+      console.log(res)
+      setUser(res.data.items)
+    })
+
+  }, [])
+
+   
 
   return (
     <div className={s.container}>
@@ -17,12 +30,11 @@ export const Github = () => {
       </div>
 
       <ul>
-        {['Diana', 'Dima', 'Veronika']
-          .map(t =>
-            <li className={selectidUser === t ? s.selected : ''} onClick={() => {
+        {users.map(t =>
+            <li key= {t.id} className={selectidUser === t ? s.selected : ''} onClick={() => {
               setSelectedUser(t);
             }}>
-              {t}
+              {t.login}
             </li>
           )}
       </ul>
